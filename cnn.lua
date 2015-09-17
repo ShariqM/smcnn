d016631faeb5f917542b8cde46855add0461e14a
Shariq Mobin <shariq.mobin@gmail.com>
2015-09-17 15:30:41 -0700

    temporal convolution

--- cnn.lua
+++ cnn.lua
@@ -17,7 +17,7 @@ if true then -- Use this until we preprocess the TIMIT data set
     trainset = {}
     -- 28 Speakers * 1000 samples, 1 channel, 16kHZ (upper bound on phoneme?)
     trainset['data'] = torch.Tensor(1000 * 28, 1, 16000)
-    trainset['label'] = :w
+    trainset['label'] = 1 -- TODO
 else
     trainset = torch.load('timit-train.t7')
     testset = torch.load('timit-test.t7')
@@ -26,20 +26,20 @@ end
 
 -- NETWORK
 num_conv_layers = 2
-filt_sizes = {5, 5}
+filt_sizes = {5,5}
 pool_sizes = {2,2}
 channels = {6,16}
 channels[0] = 1 -- Input Channels (from data)
 
 net = nn.Sequential()
-for i = 1, num_conv_layers, do
-    net:add(nn.SpatialConvolution(channels[i-1], channels[i], filt_sizes[i][0], filt_sizes[i][1])) -- MM?
+for i = 1, num_conv_layers do
+    net:add(nn.TemporalConvolution(channels[i-1], channels[i], filt_sizes[i])) -- MM ?
     net:add(nn.ReLU())
-    net:add(nn.SpatialMaxPooling(pool[i][0], pool[i][1]))
+    net:add(nn.TemporalMaxPooling(pool_sizes[i]))
 end
 
-neurons = channels[num_conv_layers] * result_size
 -- TODO
+neurons = channels[num_conv_layers]
 net:add(nn.View(neurons))
 net:add(nn.Linear(16*5*5, 120))
 net:add(nn.Linear(120, 84))
