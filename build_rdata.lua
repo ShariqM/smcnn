dea58e480fa77838134e5f52aa1464736c2f1eda
Shariq Mobin <shariq.mobin@gmail.com>
2015-10-20 18:52:40 -0700

    Pick random pairs and pull or push them apart, next up batch processing?

--- build_rdata.lua
+++ build_rdata.lua
@@ -4,7 +4,6 @@ local matio = require 'matio'
 matio.use_lua_strings = true
 
 local speaker = 'FCJF0'
-local phn = 'ix' -- 1 Phoneme for now
 
 trainset = {}
 ts = {}
@@ -12,23 +11,22 @@ ts['all'] = {}
 ts['hs']={{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}}
 
 all_data = matio.load('timit/TRAIN/PHN_SPK/phn_spk.mat' % speaker)
+min_length = math.min(poolsize + get_comp_lost(filt_sizes, 1), get_comp_lost(filt_sizes, 2))
+print ('m', min_length)
 local count = 1
 for name, data in pairs(all_data) do
     -- if string.find(name, speaker) and string.find(name, phn) then
     -- if true then
     if string.find(name, speaker) then
-        -- if data:size()[2] == 26 then
-
         tdata = data:transpose(1,2)
-        if tdata:size()[1] > 18 then
+        if tdata:size()[1] > 18 then --
             d, speaker, phn, idx = unpack(string.split(name, "_"))
             idx = tonumber(idx) + 1
-            print (phn)
+
             len = get_out_length(tdata, filt_sizes, poolsize)
             ts.all[#ts.all+1] = {tdata, phn, speaker, len}
             ts.hs[len][#ts.hs[len]+1] = count
 
-            -- print ('idx', idx, 'size', data:size()[2], 'hinge size:', tmp)
             count = count + 1
         end
     end
