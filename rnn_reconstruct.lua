392443ace1c8687adab3c6e53a683de1730bbde8
Shariq Mobin <shariq.mobin@gmail.com>
2015-10-21 18:53:11 -0700

    some part there with rnn adjustments

---
+++ rnn_reconstruct.lua
@@ -0,0 +1,256 @@
+
+--[[
+
+This file trains a character-level multi-layer RNN on text data
+
+Code is based on implementation in
+https://github.com/oxford-cs-ml-2015/practical6
+but modified to have multi-layer support, GPU support, as well as
+many other common model/optimization bells and whistles.
+The practical6 code is in turn based on
+https://github.com/wojciechz/learning_to_execute
+which is turn based on other stuff in Torch, etc... (long lineage)
+
+]]--
+
+require 'torch'
+require 'nn'
+require 'nngraph'
+require 'optim'
+require 'lfs'
+
+matio = require 'matio'
+matio.use_lua_strings = true
+local model_utils=require 'model_utils'
+local LSTM = require 'models.lstm'
+local RNN = require 'models.rnn'
+
+cmd = torch.CmdLine()
+cmd:text()
+cmd:text('Train a speech auto-encoding model')
+cmd:text()
+cmd:text('Options')
+-- data
+-- model params
+cmd:option('-rnn_size', 175, 'size of LSTM internal state')
+cmd:option('-num_layers', 3, 'number of layers in the LSTM')
+cmd:option('-model', 'lstm', 'lstm or rnn')
+-- optimization
+cmd:option('-iters',100,'iterations per epoch')
+cmd:option('-learning_rate',2e-3,'learning rate')
+cmd:option('-learning_rate_decay',0.97,'learning rate decay')
+cmd:option('-learning_rate_decay_after',10,'in number of epochs, when to start decaying the learning rate')
+cmd:option('-decay_rate',0.95,'decay rate for rmsprop')
+cmd:option('-dropout',0,'dropout for regularization, used after each RNN hidden layer. 0 = no dropout')
+cmd:option('-seq_length',50,'number of timesteps to unroll for')
+cmd:option('-batch_size',50,'number of sequences to train on in parallel')
+cmd:option('-max_epochs',50,'number of full passes through the training data')
+cmd:option('-grad_clip',5,'clip gradients at this value')
+cmd:option('-train_frac',0.95,'fraction of data that goes into train set')
+cmd:option('-val_frac',0.05,'fraction of data that goes into validation set')
+            -- test_frac will be computed as (1 - train_frac - val_frac)
+cmd:option('-init_from', '', 'initialize network parameters from checkpoint at this path')
+-- bookkeeping
+cmd:option('-seed',123,'torch manual random number generator seed')
+cmd:option('-print_every',1,'how many steps/minibatches between printing out the loss')
+cmd:option('-eval_val_every',1000,'every how many iterations should we evaluate on validation data?')
+cmd:option('-checkpoint_dir', 'cv', 'output directory where checkpoints get written')
+cmd:option('-savefile','lstm','filename to autosave the checkpont to. Will be inside checkpoint_dir/')
+cmd:option('-accurate_gpu_timing',0,'set this flag to 1 to get precise timings when using GPU. Might make code bit slower but reports accurate timings.')
+-- GPU/CPU
+cmd:option('-type',       'double', 'type: double | float | cuda')
+cmd:text()
+
+-- parse input params
+opt = cmd:parse(arg)
+torch.manualSeed(opt.seed)
+-- train / val / test split for data, in fractions
+local test_frac = math.max(0, 1 - (opt.train_frac + opt.val_frac))
+local split_sizes = {opt.train_frac, opt.val_frac, test_frac}
+
+-- CUDA
+if opt.type == 'float' then
+    print('==> switching to floats')
+    torch.setdefaulttensortype('torch.FloatTensor')
+elseif opt.type == 'cuda' then
+    print('==> switching to CUDA')
+    require 'cunn'
+    torch.setdefaulttensortype('torch.CudaTensor') -- FIXME?
+end
+
+-- Load the Training and Test Set
+cqt_features = 175
+dofile('build_data/build_pdata.lua')
+
+print('creating an ' .. opt.model .. ' with ' .. opt.num_layers .. ' layers')
+protos = {}
+if opt.model == 'lstm' then
+    protos.rnn = LSTM.lstm(cqt_features, opt.rnn_size, opt.num_layers, opt.dropout)
+elseif opt.model == 'rnn' then
+    protos.rnn = RNN.rnn(vocab_size, opt.rnn_size, opt.num_layers, opt.dropout)
+end
+protos.criterion = nn.MSECriterion()
+
+-- the initial state of the cell/hidden states
+init_state = {}
+for L=1,opt.num_layers do
+    local h_init = torch.zeros(opt.batch_size, opt.rnn_size)
+    if opt.type == 'cuda' then h_init = h_init:cuda() end
+    table.insert(init_state, h_init:clone())
+    if opt.model == 'lstm' then table.insert(init_state, h_init:clone()) end
+end
+
+-- Put everything on the GPU
+if opt.type == 'cuda' then
+    for name,proto in pairs(protos) do
+        print('cudafying '..name)
+        proto:cuda()
+    end
+end
+
+-- put the above things into one flattened parameters tensor
+params, grad_params = model_utils.combine_all_parameters(protos.rnn)
+
+-- initialization
+params:uniform(-0.08, 0.08) -- small uniform numbers
+
+-- initialize the LSTM forget gates with slightly higher biases to encourage remembering in the beginning
+if opt.model == 'lstm' then
+    for layer_idx = 1, opt.num_layers do
+        for _,node in ipairs(protos.rnn.forwardnodes) do
+            if node.data.annotations.name == "i2h_" .. layer_idx then
+                print('setting forget gate biases to 1 in LSTM layer ' .. layer_idx)
+                -- the gates are, in order, i,f,o,g, so f is the 2nd block of weights
+                node.data.module.bias[{{opt.rnn_size+1, 2*opt.rnn_size}}]:fill(1.0)
+            end
+        end
+    end
+end
+
+print('number of parameters in the model: ' .. params:nElement())
+-- make a bunch of clones after flattening, as that reallocates memory
+clones = {}
+for name,proto in pairs(protos) do
+    print('cloning ' .. name)
+    clones[name] = model_utils.clone_T_times(proto, opt.seq_length, not proto.parameters)
+end
+
+-- do fwd/bwd and return loss, grad_params
+local init_state_global = clone_list(init_state)
+function feval(x)
+    if x ~= params then
+        params:copy(x)
+    end
+    grad_params:zero()
+
+    ------------------ get minibatch -------------------
+    local start = torch.random(trainset:size()[1] - opt.seq_length)
+    x = trainset[{start,start+opt.seq_length}]
+    x = x:cuda() -- Ship to GPU | Need Float?
+
+    ------------------- forward pass -------------------
+    local rnn_state = {[0] = init_state_global}
+    local reconstructions = {}
+    local loss = 0
+
+    for t=1,opt.seq_length do
+        clones.rnn[t]:training() -- make sure we are in correct mode (this is cheap, sets flag)
+        local lst = clones.rnn[t]:forward{x[t], unpack(rnn_state[t-1])}
+        rnn_state[t] = {}
+        for i=1,#init_state do table.insert(rnn_state[t], lst[i]) end -- extract the state, without output
+        reconstructions[t] = lst[#lst] -- last element is the prediction
+        loss = loss + clones.criterion[t]:forward(reconstructions[t], x[t])
+    end
+    loss = loss / opt.seq_length
+
+    ------------------ backward pass -------------------
+    -- initialize gradient at time t to be zeros (there's no influence from future)
+    local drnn_state = {[opt.seq_length] = clone_list(init_state, true)} -- true also zeros the clones
+    for t=opt.seq_length,1,-1 do
+        -- backprop through loss, and softmax/linear
+        local doutput_t = clones.criterion[t]:backward(reconstructions[t], x[t])
+        table.insert(drnn_state[t], doutput_t)
+        local dlst = clones.rnn[t]:backward({x[t], unpack(rnn_state[t-1])}, drnn_state[t])
+        drnn_state[t-1] = {}
+        for k,v in pairs(dlst) do
+            if k > 1 then -- k == 1 is gradient on x, which we dont need
+                -- note we do k-1 because first item is dembeddings, and then follow the
+                -- derivatives of the state, starting at index 2. I know...
+                drnn_state[t-1][k-1] = v
+            end
+        end
+    end
+    ------------------------ misc ----------------------
+    -- transfer final state to initial state (BPTT)
+    init_state_global = rnn_state[#rnn_state] -- NOTE: I don't think this needs to be a clone, right?
+    -- grad_params:div(opt.seq_length) -- this line should be here but since we use rmsprop it would have no effect. Removing for efficiency
+    -- clip gradient element-wise
+    grad_params:clamp(-opt.grad_clip, opt.grad_clip)
+    return loss, grad_params
+end
+
+-- start optimization here
+train_losses = {}
+val_losses = {}
+local optim_state = {learningRate = opt.learning_rate, alpha = opt.decay_rate}
+local iterations = opt.max_epochs * opt.iters
+local iterations_per_epoch = opt.iters
+local loss0 = nil
+for i = 1, iterations do
+    local epoch = i / iterations_per_epoch
+
+    local timer = torch.Timer()
+    local _, loss = optim.rmsprop(feval, params, optim_state)
+    local time = timer:time().real
+
+    local train_loss = loss[1] -- the loss is inside a list, pop it
+    train_losses[i] = train_loss
+
+    -- exponential learning rate decay
+    if i % iterations_per_epoch == 0 and opt.learning_rate_decay < 1 then
+        if epoch >= opt.learning_rate_decay_after then
+            local decay_factor = opt.learning_rate_decay
+            optim_state.learningRate = optim_state.learningRate * decay_factor -- decay it
+            print('decayed learning rate by a factor ' .. decay_factor .. ' to ' .. optim_state.learningRate)
+        end
+    end
+
+    -- every now and then or on last iteration
+    if false and (i % opt.eval_val_every == 0 or i == iterations) then
+        -- evaluate loss on validation data
+        local val_loss = eval_split(2) -- 2 = validation
+        val_losses[i] = val_loss
+
+        local savefile = string.format('%s/lm_%s_epoch%.2f_%.4f.t7', opt.checkpoint_dir, opt.savefile, epoch, val_loss)
+        print('saving checkpoint to ' .. savefile)
+        local checkpoint = {}
+        checkpoint.protos = protos
+        checkpoint.opt = opt
+        checkpoint.train_losses = train_losses
+        checkpoint.val_loss = val_loss
+        checkpoint.val_losses = val_losses
+        checkpoint.i = i
+        checkpoint.epoch = epoch
+        checkpoint.vocab = loader.vocab_mapping
+        torch.save(savefile, checkpoint)
+    end
+
+    if i % opt.print_every == 0 then
+        print(string.format("%d/%d (epoch %.3f), train_loss = %6.8f, grad/param norm = %6.4e, time/batch = %.4fs", i, iterations, epoch, train_loss, grad_params:norm() / params:norm(), time))
+    end
+
+    if i % 10 == 0 then collectgarbage() end
+
+    -- handle early stopping if things are going really bad
+    if loss[1] ~= loss[1] then
+        print('loss is NaN.  This usually indicates a bug.  Please check the issues page for existing issues, or create a new issue, if none exist.  Ideally, please state: your operating system, 32-bit/64-bit, your blas version, cpu/cuda/cl?')
+        break -- halt
+    end
+    if loss0 == nil then loss0 = loss[1] end
+    if loss[1] > loss0 * 3 then
+        print('loss is exploding, aborting.')
+        break -- halt
+    end
+end
+
+
