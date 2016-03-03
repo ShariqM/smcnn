9dee9ff19034a32e858009a0d1bcf6af03a92f89
Shariq Mobin <Shariq.Mobin@gmail.com>
2016-03-02 22:19:30 -0800

    avg stat

--- splitter.py
+++ splitter.py
@@ -27,7 +27,15 @@ def get_alignment(speaker, fname):
     f.close()
     return words
 
+def insert(t, key, val):
+    if not t.has_key(key):
+        t[key] = [val]
+    else:
+        t[key].append(val)
 
+
+
+stats = {}
 for speaker in (1,2):
     fc = open('metadata/s%d_catalog.txt' % speaker)
     x = fc.readline()
@@ -35,7 +43,6 @@ for speaker in (1,2):
     while x != '':
         fname = x[:-1]
         words = get_alignment(speaker, fname)
-        print words
 
         mname = 'grid/grid/s%d/mat/s%d_%d.mat' % (speaker, speaker, i)
         cqtv = sio.loadmat(mname)['X']
@@ -44,7 +51,14 @@ for speaker in (1,2):
             cqt_word = np.zeros((cqtv.shape[0], cqtv.shape[1]))
             cqt_word[:,start:stop] = cqtv[:, start:stop]
 
+            insert(stats, '', 0. +  stop - start)
+
         x = fc.readline()
         i = i + 1
+
+    avgs = {}
+    for key in stats.keys():
+        print "Word: %s, Avg=%.2f, STD=%.2f" % (key, np.average(stats[key]), np.std(stats[key]))
+        pdb.set_trace()
     fc.close()
 
