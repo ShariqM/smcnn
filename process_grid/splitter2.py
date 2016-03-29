02fcaf3e84fd30c354e6879466de139463e20280
Shariq Mobin <Shariq.Mobin@gmail.com>
2016-03-28 19:09:27 -0700

    process working!

--- process_grid/splitter2.py
+++ process_grid/splitter2.py
@@ -16,16 +16,24 @@ for spk in range(1,34):
         aname = fname.split('/')[-1][:-4]
         words = process_align('grid/data/all_align/s%d_align/%s.align' % (spk, aname))
 
-        #y, sr = librosa.load(fname)
-        sr, y = wavfile.read(fname)
+        rosa = True
+        if rosa:
+            y, sr = librosa.load(fname, sr=25000)
+            #y, sr = librosa.load(fname)
+        else:
+            sr, y = wavfile.read(fname)
         for (word, start, stop) in words:
             print word, start, stop
             y_word = y[start:min(y.shape[0],stop)]
             stft_matrix = librosa.stft(y_word)
-            y_hat       = librosa.istft(stft_matrix)
+            stft_matrix.imag = np.zeros((stft_matrix.shape[0], stft_matrix.shape[1]))
+            y_word      = librosa.istft(stft_matrix)
 
-            #librosa.output.write_wav('test/test_%s.wav' % word, y_word, sr)
-            wavfile.write('test/test_%s.wav' % word, sr, y_hat)
+
+            if rosa:
+                librosa.output.write_wav('test/rtest_%s.wav' % word, y_word, sr)
+            else:
+                wavfile.write('test/wtest_%s.wav' % word, sr, y_word)
         pdb.set_trace()
 
 
